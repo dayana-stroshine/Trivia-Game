9b6927bf775cad489aa1346af51252ef4d64477b
dvastro <stroshid@oregonstate.edu>
2017-10-02 17:40:08 -0500

    randomly generate questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -53,14 +53,20 @@
 // var quiz = new Quiz(questions);
 
 // populate();
+
+// functon questionMatch
+// document.getElementById("btn0").innerHTML = answersA["Question A"]; 
+// document.getElementById("btn1").innerHTML = answersA["Question A"]; 
+// document.getElementById("btn2").innerHTML = answersA["Question A"]; 
+// document.getElementById("btn3").innerHTML = answersA["Question A"]; 
 //********************************* Start Button ******************************************* //
 $('#startBtn').on('click', function(){
 	$(".jumbotron").css("display", "none");
 	$(".layout").css("display", "block");
 })
 
-//********************************* Questions Array ******************************************* //
-var questions = ["Question A", "Question B", "Question C","Question D","Question E"];
+//********************************* Questions Placement *********************************** //
+var questions= ["Chicago is an Indian word from which the name of the city is derived. What does the word mean?","A section of which Chicago Street is known as 'the magnificent mile?'", "Question C","Question D","Question E"];
 
 function shuffleArray(questions) {
     for (var i = questions.length -1; i > 0; i--) {
@@ -76,18 +82,63 @@ function shuffleArray(questions) {
 shuffleArray(questions);
 document.getElementById("question").innerHTML = questions[0];
 
-//********************************* Answers Array ******************************************* //
-var answersA = ["Answer A", "Answer B", "Answer C","Answer D"];
+//********************************* Answers Placement *************************************** //
+var answersA= ["The name of a Potawatomi Indian chief", "City by the lake", "Wild leek, or skunk cabbage","Marshy place"];
 
-function showAnswer(answers) {
-    if(questions[0] === questions[1])
-    return answersA;
-    
+function answerFillA(answersA) {
+document.getElementById("btn0").innerHTML = answersA[0]; 
+document.getElementById("btn1").innerHTML = answersA[1]; 
+document.getElementById("btn2").innerHTML = answersA[2]; 
+document.getElementById("btn3").innerHTML = answersA[3]; 
 }
 
-showAnswer(answersA);
-document.getElementById("btn0").innerHTML = answersA[0];
-document.getElementById("btn1").innerHTML = answersA[1];
-document.getElementById("btn2").innerHTML = answersA[2];
-document.getElementById("btn3").innerHTML = answersA[3];
+var answersB= ["Randolph Street", "Michigan Avenue", "State Street","Wabash Avenue"];
+
+function answerFillB(answersB) {
+document.getElementById("btn0").innerHTML = answersB[0]; 
+document.getElementById("btn1").innerHTML = answersB[1]; 
+document.getElementById("btn2").innerHTML = answersB[2]; 
+document.getElementById("btn3").innerHTML = answersB[3]; 
+}
+
+var answersC= ["The name of a Potawatomi Indian chief", "City by the lake", "Wild leek, or skunk cabbage","Marshy place"];
+
+function answerFillC(answersC) {
+document.getElementById("btn0").innerHTML = answersC[0]; 
+document.getElementById("btn1").innerHTML = answersC[1]; 
+document.getElementById("btn2").innerHTML = answersC[2]; 
+document.getElementById("btn3").innerHTML = answersC[3]; 
+}
+
+var answersD= ["The name of a Potawatomi Indian chief", "City by the lake", "Wild leek, or skunk cabbage","Marshy place"];
+
+function answerFillD(answersD) {
+document.getElementById("btn0").innerHTML = answersD[0]; 
+document.getElementById("btn1").innerHTML = answersD[1]; 
+document.getElementById("btn2").innerHTML = answersD[2]; 
+document.getElementById("btn3").innerHTML = answersD[3]; 
+}
+
+var answersE= ["The name of a Potawatomi Indian chief", "City by the lake", "Wild leek, or skunk cabbage","Marshy place"];
+
+function answerFillE(answersE) {
+document.getElementById("btn0").innerHTML = answersE[0]; 
+document.getElementById("btn1").innerHTML = answersE[1]; 
+document.getElementById("btn2").innerHTML = answersE[2]; 
+document.getElementById("btn3").innerHTML = answersE[3]; 
+}
+//*********************** Matching Q & A ***************************************************** //
+function matchQA(){
+    if(questions === "Chicago is an Indian word from which the name of the city is derived. What does the word mean?"){
+        answerFillA();
+    }
+    else if(questions === "A section of which Chicago Street is known as 'the magnificent mile?'"){
+        answerFillB();
+
+    }
+}
+
+matchQA();
+
+
 
